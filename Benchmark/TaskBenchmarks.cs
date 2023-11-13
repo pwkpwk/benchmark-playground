818ad3ad66b3ec864c66a2f36fd38ec844fd241c
Pavel Karpenko <pwkpwk@hotmail.com>
2023-11-13 09:25:25 -0800

    Add tsak benchmarks

---
+++ Benchmark/TaskBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AmbientBytes.Benchmark;
+
+public class TaskBenchmarks
+{
+    [Benchmark]
+    public async Task AwaitInAsync()
+    {
+        await DoubleAsync();
+    }
+
+    [Benchmark]
+    public async Task AwaitInAsyncConfigureAwait()
+    {
+        await DoubleAsync().ConfigureAwait(false);
+    }
+
+    [Benchmark]
+    public async Task ReturnTask()
+    {
+        await NoAsync();
+    }
+
+    private async Task DoubleAsync()
+    {
+        await Task.Delay(0);
+    }
+
+    private Task NoAsync()
+    {
+        return Task.Delay(0);
+    }
+}
