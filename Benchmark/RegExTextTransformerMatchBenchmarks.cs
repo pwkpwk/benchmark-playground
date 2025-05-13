9481356e9675625ef30b61bb218cd2dc011051a2
Pavel Karpenko <pwkpwk@hotmail.com>
2025-05-12 17:24:21 -0700

    More benchmarks

--- Benchmark/RegExTextTransformerMatchBenchmarks.cs
+++ Benchmark/RegExTextTransformerMatchBenchmarks.cs
@@ -8,11 +8,14 @@ namespace AmbientBytes.Benchmark;
 public class RegExTextTransformerMatchBenchmarks
 {
     private IDictionary<string, string> _variables;
+    private Regex _raw;
+    private Regex _rawCompiled;
     private ITextTransformer _interpreted;
     private ITextTransformer _compiled;
 
     private const string Expression = @"^(\s*)https://(?<a>benchmark).example.com/path/(?<b>beta)[^\s]*(\s*)$";
     private const string Input = "   https://benchmark.example.com/path/beta?q=query   ";
+    private const string Replacement = "https://$(a).example.com/path/$(b)?q=query";
 
     [Params(1, 1000)] public int Repetitions;
 
@@ -20,11 +23,35 @@ public class RegExTextTransformerMatchBenchmarks
     public void SetUp()
     {
         _variables = new Dictionary<string, string> { { "a", "-A-" }, { "b", "-B-" }, { "c", "-C-" } };
+        _raw = new Regex(Expression);
+        _raw = new Regex(Expression, RegexOptions.Compiled);
         _interpreted = new RegExTextTransformer(Expression, _variables, RegexOptions.None);
         _compiled = new RegExTextTransformer(Expression, _variables, RegexOptions.Compiled);
     }
 
     [Benchmark(Baseline = true)]
+    public string RegexReplace()
+    {
+        for (int i = 0; i < Repetitions; ++i)
+        {
+            _ = _raw.Replace(Input, Replacement);
+        }
+
+        return _raw.Replace(Input, Replacement);
+    }
+
+    [Benchmark]
+    public string RegexCompiledReplace()
+    {
+        for (int i = 0; i < Repetitions; ++i)
+        {
+            _ = _rawCompiled.Replace(Input, Replacement);
+        }
+
+        return _rawCompiled.Replace(Input, Replacement);
+    }
+
+    [Benchmark]
     public string InterpretedExpression()
     {
         for (int i = 0; i < Repetitions; ++i)
